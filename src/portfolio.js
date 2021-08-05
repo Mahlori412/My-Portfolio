d2cd78eeb0ff97ef36e40efa160ce11bad5859b4
Mahlori412 <manganyimahlori6@gmai.com>
2021-08-05 14:54:57 +0200

    my portfolio

---
+++ src/portfolio.js
@@ -0,0 +1,209 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+  name: "Mahlori Manganyi",
+  title: "Hi all, I'm Mahlori",
+  description:
+    "I am a qualified software developer to build on an extensive range of technical skills within a suitably challenging role to bridge the gap between theoretical & practical knowledge and experience in the world of technology.",
+  resumeLink:
+    "https://drive.google.com/file/d/1RVqqItgbH9xt4U3k26tveQ-ywRZeiftS/view?usp=sharing",
+};
+
+export const openSource = {
+  githubUserName: "Mahlori412",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+  facebook: "https://www.facebook.com/mahlori2/",
+  instagram: "https://www.instagram.com/manganyimahlori/",
+  twitter: "https://twitter.com/manganyimahlori",
+  github: "https://github.com/Mahlori412",
+  linkedin: "https://www.linkedin.com/in/mahlori-manganyi-568a111bb/",
+};
+
+export const skillsSection = {
+  title: "What I do",
+  subTitle: "My goal is to apply my technical expertise all throughout the full software life cycle to ensure production and delivery of products and services that meet client specifications. Along with a competent software developing team, and with strong personal knowledge, skills, and experience in software engineering, I am positive that this goal can be achieved. My experience as junior software developer enhanced my abilities in designing, implementing, testing, and upgrading software. One of my objectives is to keep updated with the latest IT trends and technologies. I am confident that if given the opportunity, I can be useful to the company",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web applications"
+    ),
+    emoji("⚡ Develop technical specifications and coding documentations for new projects."),
+    emoji("⚡ Work with Business Analyst and Architects to develop applications based on project requirements."),
+    emoji("⚡ Worked with Business Analyst and Architects to develop applications based on project requirements."),
+    emoji("⚡ Understanding of database design, version control concepts and the relational database"),
+    emoji("⚡ Knowledge of client/server application development and agile development methodologies"),
+  ],
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "vscode-icons:file-type-html",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "vscode-icons:file-type-css",
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "logos:sass",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "logos:javascript",
+    },
+    {
+      skillName: "TypeScript",
+      fontAwesomeClassname: "logos:typescript-icon",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "logos:nodejs-icon",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "vscode-icons:file-type-npm",
+    },
+    {
+      skillName: "Postgresql",
+      fontAwesomeClassname: "logos:postgresql",
+    },
+    {
+      skillName: "mongoDB",
+      fontAwesomeClassname: "vscode-icons:file-type-mongo",
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "logos:aws",
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "logos:firebase",
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "logos:python",
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "logos:git-icon",
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "logos:docker-icon",
+    },
+  ],
+};
+
+export const SkillBars = [
+  {
+    Stack: "Frontend Development", //Insert stack or technology you have experience in
+    progressPercentage: "60", //Insert relative proficiency in percentage
+  },
+  {
+    Stack: "Backend",
+    progressPercentage: "70",
+  },
+  {
+    Stack: "Programming",
+    progressPercentage: "70",
+  },
+];
+
+export const educationInfo = [
+  {
+    schoolName: "Tshwane University of Technology",
+    subHeader: "National Diploma in Software Development",
+    duration: "January 2015 - December 2020",
+    desc: "2015-2019 Theory Modules",
+    descBullets: [
+      "Planning to do advanced diploma in 2022",
+    ],
+  },
+  {
+    schoolName: "Risinga High School",
+    duration: "January 2009 - December 2013",
+    desc: "SUBJECTS: English First Additional Language, Xitsonga Home Language, Mathematics, Life Orientation, Agricultural Sciences, Life Sciences, Physical Sciences",
+    // descBullets: [
+    //   "Planning to do my honours in Cyber Security",
+    //   "End goal is being a Software Security pentester",
+    // ],
+  },
+];
+
+export const experience = [
+  {
+    role: "Software Developer Intern",
+    date: "Janaury 2020 - August 2020",
+    desc: "I worked as a Software Developer intern",
+    descBullets: [
+      "During the internship I learned and gain more knowledge in the world of technology",
+      "We managed to build more web and mobile applications to help the community and students",
+      "ICEP website: https://www.icep.co.za/",
+    ],
+  },
+  {
+    role: "Freelancer at anointed stokvel club",
+    date: "Janaury 2020 - present",
+    desc: "Working as back-end Developer",
+    descBullets: [
+      "We building web application and mobile application for companies dealing financials",
+      "Programming langauges mostly used Nodejs, Postgresql, and C#",
+      "Anointed stokvel club website: https://anointedstokvelclub.com/",
+    ],
+  },
+];
+
+export const projects = [
+  {
+    name: "ICEP",
+    desc: "Web application that helps students to apply for internships(Nodejs, Angularjs and MongoDB)",
+    link: "https://www.icep.co.za/",
+  },
+  {
+    name: "Anointed Stockvel club",
+    desc: "Website for business to partner with other businesses",
+    link: "https://anointedstokvelclub.com/",
+  },
+  {
+    name: "Declaration system",
+    desc: [
+      "System to help student to deeclare their devices such as laptop, fridge or any other device that has a serial number to ensure that they are known in TUT campuses. ",
+      "Under construction 70% done"
+    ],
+  },
+];
+
+export const feedbacks = [
+  {
+    name: "Kagiso - 🇳🇬",
+    feedback:
+      "He is very patient and intelligent and always pay attention to customers requirement also i like the honesty in him through his processes of getting jobs done. Most definitely i recommend him for your next project",
+  },
+  {
+    name: "Nyambeni - 🇵🇰",
+    feedback:
+      "He is amazing teacher for the react and node development. If i say mern stack development it would be great. More patient and skilled person. Really amazing. Highly recommended.",
+  },
+  {
+    name: "Vutlhari Mhangwani - 🇪🇸",
+    feedback:
+      "He is brilliant! He makes sure that he is providing the best solution for the client and explains the job done in an easy way to understand. I felt really comfortable with him.",
+  },
+  {
+    name: "Ntshembo - 🇮🇳",
+    feedback:
+      "Its great to work with Ntshembo. Good in communication and faster results. Thank you",
+  },
+  {
+    name: "Themba Ngobese - 🇫🇷",
+    feedback:
+      "Did an incredible work in a short time, was really kind and helpful, amazing communication skills. He took time to explain me in depth what he did and how was it done. Explanations were really great and showed that he had incredible technological background, helped me understand what the code and what was happening in the back seen. Very skillful. Really enjoyed working with him. Recommand him 200%",
+  },
+];
